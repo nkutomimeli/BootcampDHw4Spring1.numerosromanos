c8c817209b54f08502994b8cfdaee0f0f0df313e
nkutomimeli <nathan.kutomi@mercadolivre.com>
2021-12-27 14:33:11 -0300

    Initial commit

---
+++ src/test/java/com/bootcampdhw4spring1/numerosromanos/NumerosromanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcampdhw4spring1.numerosromanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumerosromanosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
